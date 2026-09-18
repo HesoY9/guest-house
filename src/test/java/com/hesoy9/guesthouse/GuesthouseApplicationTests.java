e480d0d56e1ac6671f615fba51bfe044b5d6b4fd
Sajith <it25100449@my.sliit.lk>
2026-09-18 19:19:57 +0900

    spring initialized

---
+++ src/test/java/com/hesoy9/guesthouse/GuesthouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hesoy9.guesthouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuesthouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
